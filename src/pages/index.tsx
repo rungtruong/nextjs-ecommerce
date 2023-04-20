58e433b7537ff72d03d673a6c62e8eab79f99ce5
rungtruong <rung.iuh@gmail.com>
2023-04-20 11:36:46 +0700

    refactor: refactor webstorm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,8 @@ import { signOut, useSession } from 'next-auth/react';
 
 import { MetaTag } from '@/components';
 import LoginPage from '@/components/common/login-page';
-
-import type { NextPageWithLayout } from '../models/common';
-import { AppConfig } from '../utils/app-config';
+import type { NextPageWithLayout } from '@/models';
+import { AppConfig } from '@/utils/app-config';
 
 const Index: NextPageWithLayout = () => {
   const { data: session } = useSession();
